423909645252279c605abc20ebcd8610f0ac5522
Douglas Moraes <douglasgm89@live.com>
2024-10-28 21:30:44 -0300

    feat: Users Routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productRouter from '@modules/products/routes/ProductsRoutes';
+import usersRouter from '@modules/users/routes/UserRoutes';
 import { response, Router } from 'express';
 
 const routes = Router();
@@ -6,5 +7,7 @@ const routes = Router();
 routes.get('/health', (request, response) => {
   return response.json({ message: 'Henlo dev! I am Aliveeeee' });
 });
+routes.use('/users', usersRouter);
 routes.use('/products', productRouter);
+
 export default routes;
