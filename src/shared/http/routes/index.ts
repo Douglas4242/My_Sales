fbdfeacab26094108344278fcd36371a9c216480
Douglas Moraes <douglasgm89@live.com>
2024-10-29 21:21:13 -0300

    feat: sessions

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productRouter from '@modules/products/routes/ProductsRoutes';
+import sessionsRouter from '@modules/users/routes/SessionRoutes';
 import usersRouter from '@modules/users/routes/UserRoutes';
 import { response, Router } from 'express';
 
@@ -7,7 +8,8 @@ const routes = Router();
 routes.get('/health', (request, response) => {
   return response.json({ message: 'Henlo dev! I am Aliveeeee' });
 });
-routes.use('/users', usersRouter);
 routes.use('/products', productRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter)
 
 export default routes;
